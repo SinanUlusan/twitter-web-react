ace66432cb05ea5c774d8356b2db3f1de72f7954
Sinan Ulusan <sinan.ulusan@tazebt.com>
2022-06-10 19:32:29 +0200

    tema renkleri ve destegi eklendi, context api kullanıldı.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0e5d7214e977157"; }
+/******/ 		__webpack_require__.h = function() { return "6331beb79ac2b0c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
