3b2019c3867fe009cea81acb1c7c5ab99d0eb094
Sinan Ulusan <sinan.ulusan@tazebt.com>
2022-06-14 18:46:17 +0200

    ana sayfadaki loading svg olarak yenisi eklenip ortalandı

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "919819282a9344cb"; }
+/******/ 		__webpack_require__.h = function() { return "01159722c1fc05f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import useSWR from "swr";
 
+import styles from "./index.module.css";
 import Layout from "../components/layout";
 import fetcher from "../lib/fetch";
 import Tweet from "../components/tweet/index";
+import Loading from "../components/loading";
 
 function HomePage() {
   const { data, error } = useSWR("/api/tweet", fetcher);
   return (
     <Layout>
       {error && <p>An error has occured</p>}
-      {!data && <p>Loading</p>}
+      {!data && (
+        <div className={styles.loading}>
+          <Loading />
+        </div>
+      )}
 
       {data?.statuses.map((tweet) => {
         return <Tweet key={tweet.id} {...tweet}></Tweet>;
