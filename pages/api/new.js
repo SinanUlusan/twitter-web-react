f08f49b2d9b8fec1d421c9a1652ec9af36d0436e
Sinan Ulusan <sinan.ulusan@tazebt.com>
2022-06-17 00:16:03 +0200

    benim adıma tweet gönderilmemesi için yoruma alındı

--- pages/api/new.js
+++ pages/api/new.js
@@ -1,18 +1,22 @@
 import Twit from "../../lib/twit";
 
 export default function handler(req, res) {
-  const { tweet } = req.body;
+  res.status(200).json({ message: "Tweet gonderme basarili!" });
 
-  if (!tweet) return res.status(400).json({ message: "Tweet boş olamaz!" });
+  // Benim adıma tweet gönderilmesin diye bu kısmı yoruma aldım.
 
-  Twit.post(
-    "statuses/update",
-    { status: "TEST " + tweet + " sent from @sinanisscott" },
-    function (err, data, response) {
-      if (err) {
-        return res.status(400).json(data);
-      }
-      res.status(200).json(data);
-    }
-  );
+  // const { tweet } = req.body;
+
+  //if (!tweet) return res.status(400).json({ message: "Tweet boş olamaz!" });
+
+  //Twit.post(
+  //"statuses/update",
+  //{ status: "TEST " + tweet + " sent from @sinanisscott" },
+  //function (err, data, response) {
+  //if (err) {
+  //return res.status(400).json(data);
+  //}
+  //res.status(200).json(data);
+  //}
+  //);
 }
